d626bac142cfa7b3fc341a3f89e801d1db3d59fe
Kirill777-web <ki.lazutin@gmail.com>
2024-02-24 14:31:00 -0400

    aded images and added new menu component items

--- little-lemon/src/components/menu.js
+++ little-lemon/src/components/menu.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Appetizers from './appetizers';
+import { appetizers } from './menuItems'; // Ensure this import path is correct
 
 const Menu = () => {
   return (
-    <header className="header">
-      <h1 className='title'>Little Lemon </h1>
-      <p className='sub-title'>New Brunswick</p>
-      <p className='p-description'>Our mission is to provide you with the best dining experience. We offer a variety of dishes and drinks that are sure to satisfy your taste buds. We look forward to serving you!</p>
-      <Link to="/reservations" className='btn'>Reserve a Table</Link>
-      <img src="/assets/menu.jpg" alt="logo" />
-    </header>
-  )
-}
+    <main>
+      <header className="header">
+        <h1 className='title'>Little Lemon</h1>
+        <p className='sub-title'>New Brunswick</p>
+        <p className='p-description'>Our mission is to provide you with the best dining experience. We offer a variety of dishes and drinks that are sure to satisfy your taste buds. We look forward to serving you!</p>
+        <Link to="/reservations" className='btn'>Reserve a Table</Link>
+        <img src="/assets/menu.jpg" alt="logo" />
+      </header>
+      <Appetizers items={appetizers} />
+    </main>
+  );
+};
 
 export default Menu;
---
+++ little-lemon/src/components/menuItems.js
@@ -0,0 +1,30 @@
+export const appetizers = [
+  {
+    id: 'app-1',
+    title: 'Bruschetta',
+    description: 'Toasted bread with tomato, basil, and fresh mozzarella.',
+    price: '$5.99',
+    imgSrc: '/assets/bruschetta.jpg',
+  },
+  {
+    id: 'app-2',
+    title: 'Stuffed Mushrooms',
+    description: 'Mushrooms filled with herbed cream cheese, topped with Parmesan.',
+    price: '$7.99',
+    imgSrc: '/assets/stuffed-mushrooms.jpg',
+  },
+  {
+    id: 'app-3',
+    title: 'Garlic Knots',
+    description: 'Knots of pizza dough baked to perfection, served with garlic and marinara sauce.',
+    price: '$4.99',
+    imgSrc: '/assets/garlic-knots.jpg',
+  },
+  {
+    id: 'app-4',
+    title: 'Caprese Salad',
+    description: 'Sliced fresh mozzarella, tomatoes, and basil, seasoned with salt and olive oil.',
+    price: '$6.99',
+    imgSrc: '/assets/caprese-salad.jpg',
+  }
+];
